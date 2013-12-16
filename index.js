bc4f32976a0b954b914a8cfd92b8977e7d03549a
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-16 13:28:29 -0500

    initing tmp dir and running jsdoc

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 'use strict';
 
 var log        =  require('npmlog')
+  , runnel     =  require('runnel')
+  , path       =  require('path')
   , runJsdoc   =  require('./lib/run-jsdoc')
+  , initTmpDir =  require('./lib/init-tmp-dir')
+  , docmeify   =  require('./lib/docmeify')
 
-var go = module.exports = function (args, jsdocargs, cb) {
+var go = module.exports = 
+
+/**
+ * Generates jsdocs for non-private members of the project in the current folder.
+ * It then updates the given README with the githubified version of the generated API docs.
+ * 
+ * @name docme
+ * @memberof Public
+ * @function
+ * @param {String} readme path to readme in which the API docs should be updated
+ * @param {Array.<String>} args consumed by docme
+ * @param {String=} args.loglevel (info) level at which to log: silly|verbose|info|warn|error|silent
+ * @param {Array.<String>} jsdocargs consumed by jsdoc
+ * @param {Function(Error)} cb called back when docme finished updating the README
+ */
+function docme(readme, args, jsdocargs, cb) {
   args = args || {};
   jsdocargs = jsdocargs || [];
 
   log.level = args.loglevel || 'info';  
   
-  var projectRoot = args.projectRoot || process.cwd();
+  var projectRoot = args.projectRoot || process.cwd()
+    , projectName = path.basename(projectRoot)
 
-  // TODO: create tmp dir, run jsdoc in project root -- out to tmp dir, mutiny -> concat results -> update readme section
+  // TODO: mutiny -> concat results -> update readme section
+
+  initTmpDir(projectName, function (err, tmpdir) {
+    if (err) return cb(err);
+    
+    var tasks = [
+        runJsdoc.bind(null, projectRoot, tmpdir, jsdocargs)
+      , docmeify
+      , cb
+    ]
+    runnel(tasks);
+  })
+}
+
+// Test
+if (!module.parent && typeof window === 'undefined') {
+  go('', { loglevel: 'silly' }, [], function (err, res) {
+    if (err) return console.error(err);
+    console.log('Success!\n', res);  
+  });
 }
